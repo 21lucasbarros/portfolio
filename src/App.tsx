884b30b4c8093714d487866f6c357cde96ba717a
Lucas Barros <lucasweacked21@gmail.com>
2025-04-05 01:49:18 -0300

    refactor: remocao do componente Stacks.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
-import Stacks from "./components/Stacks/Stacks";
 
 export default function App() {
   return (
@@ -14,7 +13,6 @@ export default function App() {
         <main>
           <Home />
           <Education />
-          <Stacks />
           <Projects />
           <Contact />
         </main>
